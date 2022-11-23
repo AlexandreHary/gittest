7ad23c2cd0222380c8f6350d2fccfcae36fdaac8
AlexandreHary <alexandre.harry10@gmail.com>
2022-11-23 15:58:08 +0100

    merciamaury

---
+++ mainlanceur.java
@@ -0,0 +1,10 @@
+package fr.isika.cda22.coursgit;
+
+public class mainlanceur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello word");
+	}
+
+}
